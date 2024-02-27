9948137b9374069b4d7551e8f55d60c9b2f3d7ff
Agles <20223tn005@utez.edu.mx>
2024-02-27 14:19:28 -0600

    Creamos el avatar

--- src/modules/auth/adapters/screens/Profile.jsx
+++ src/modules/auth/adapters/screens/Profile.jsx
@@ -1,20 +1,35 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Button } from '@rneui/base'
+import { Button,Avatar } from '@rneui/base'
 import { getAuth } from 'firebase/auth'
 import Loading from '../../../../kernel/components/Loading'
 
 const Profile = () => {
-    const auth = getAuth();
-    
+  const auth = getAuth();
+
   return (
     <View style={styles.container}>
-      <Button 
-      title='Cerrar Sesión'
-      containerStyle={styles.Btncontainer}
-      buttonStyle={styles.BtnStyle}
-      titleStyle={{color:'black'}}
-      onPress={() => auth.signOut()}
+      <View style={styles.row}>
+        <Avatar
+          size={64}
+          rounded
+          source={{ uri: 'https://randomuser.me/api/portraits/women/57.jpg' }}
+          title="Bj"
+          containerStyle={{ backgroundColor: 'grey' }}
+        >
+          <Avatar.Accessory size={23} />
+        </Avatar>
+        <View style={{flexDirection: 'column', marginLeft: 16}}>
+          <Text style={{fontSize:14, fontWeight: 'bold'}}>Agles Avelar Ocmapo</Text>
+          <Text style={{fontSize:12}}>AglesAv@utez.edu.mx</Text>
+        </View>
+      </View>
+      <Button
+        title='Cerrar Sesión'
+        containerStyle={styles.Btncontainer}
+        buttonStyle={styles.BtnStyle}
+        titleStyle={{ color: 'black' }}
+        onPress={() => auth.signOut()}
       />
     </View>
   )
@@ -23,19 +38,22 @@ const Profile = () => {
 export default Profile
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 32,
-        backgroundColor: '#FFF'
-    },
-    Btncontainer:{
-        borderRadius: 16,
-        width: '80%',
-    },
-    BtnStyle:{
-        backgroundColor:'tomato',
-        color:'black'
-    }
+  container: {
+    flex: 1,
+    padding: 32,
+    backgroundColor: '#FFF'
+
+  },
+  Btncontainer: {
+    borderRadius: 16,
+    width: '80%',
+  },
+  BtnStyle: {
+    backgroundColor: 'tomato',
+    color: 'black'
+  },
+  row: {
+    flexDirection: 'row',
+    padding: 16
+  }
 })
