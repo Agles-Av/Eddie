7573a88fd7aa6b5532644c90e567fc65a5017cde
Agles <20223tn005@utez.edu.mx>
2024-02-27 14:25:06 -0600

    se hizo el useEffect

--- src/modules/auth/adapters/screens/Profile.jsx
+++ src/modules/auth/adapters/screens/Profile.jsx
@@ -1,11 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Button,Avatar } from '@rneui/base'
 import { getAuth } from 'firebase/auth'
 import Loading from '../../../../kernel/components/Loading'
 
 const Profile = () => {
+  const [user, setUser] =  useState(null);
   const auth = getAuth();
+
+  useEffect(()=>{
+    const profile = auth.currentUser;
+    if (profile!=null) {
+      setUser(profile);
+    }
+  },[]);
 
   return (
     <View style={styles.container}>
@@ -20,8 +28,8 @@ const Profile = () => {
           <Avatar.Accessory size={23} />
         </Avatar>
         <View style={{flexDirection: 'column', marginLeft: 16}}>
-          <Text style={{fontSize:14, fontWeight: 'bold'}}>Agles Avelar Ocmapo</Text>
-          <Text style={{fontSize:12}}>AglesAv@utez.edu.mx</Text>
+          <Text style={{fontSize:14, fontWeight: 'bold'}}>{user?.displayName || 'Anonimo'}</Text>
+          <Text style={{fontSize:12}}>{user?.email || "no hay correo electronico"}</Text>
         </View>
       </View>
       <Button
