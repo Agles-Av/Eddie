2dae7376409f24b4de799142dee975f92b8d12f0
Agles <20223tn005@utez.edu.mx>
2024-02-28 13:36:08 -0600

    Configuramos getStorage para usarlo

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navigation from "./src/modules/navigation/Navigation";
-import {app,auth,db} from './src/config/util/firebaseConnection';
+import {app,auth,db,storage} from './src/config/util/firebaseConnection';
 
 export default function App() {
   return (
